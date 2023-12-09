342977b3487edd1c7146b8075cbafb8f46a83ab9
adefaze <adefaze@gmail.com>
2023-12-09 03:52:32 +0100

    added storage section

--- lib/views/team_folder.dart
+++ lib/views/team_folder.dart
@@ -8,8 +8,12 @@ class TeamFolderPage extends StatefulWidget {
 }
 
 class _TeamFolderPageState extends State<TeamFolderPage> {
+  double availableScreenSize = 0;
+
   @override
   Widget build(BuildContext context) {
+    availableScreenSize = MediaQuery.of(context).size.width - 50;
+
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: Column(children: [
@@ -20,7 +24,8 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
           ),
           alignment: Alignment.bottomCenter,
           height: 172,
-          decoration: BoxDecoration(color: Colors.blue[900]),
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 5, 80, 192)),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -81,8 +86,82 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
               ),
             ],
           ),
+        ),
+        const SizedBox(height: 25),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RichText(
+                text: const TextSpan(
+                  text: 'Storage ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: '0.5/10GB',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 16,
+                        )),
+                  ],
+                ),
+              ),
+              const Text(
+                'Upgrade',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 18),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            children: [
+              buildFileSizeChart('SOURCES', Colors.green, .3),
+              const SizedBox(width: 2),
+              buildFileSizeChart('DOCS', Colors.red, .25),
+              const SizedBox(width: 2),
+              buildFileSizeChart('IMAGES', Colors.orange, .20),
+              const SizedBox(width: 2),
+              buildFileSizeChart('', Colors.grey[200], .23),
+            ],
+          ),
         )
       ]),
+    );
+  }
+
+  Column buildFileSizeChart(
+      String title, Color? color, double widthPercentage) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 4,
+          width: availableScreenSize * widthPercentage,
+          decoration: BoxDecoration(color: color),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
