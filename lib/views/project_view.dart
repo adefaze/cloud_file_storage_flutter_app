eadfbb4a49809a41d9c01049a7b5385a03af1dc1
adefaze <adefaze@gmail.com>
2023-12-12 00:53:19 +0100

    added file project view

--- lib/views/project_view.dart
+++ lib/views/project_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ProjectPage extends StatefulWidget {
   final String folderName;
+
   const ProjectPage({super.key, required this.folderName});
 
   @override
@@ -9,6 +10,8 @@ class ProjectPage extends StatefulWidget {
 }
 
 class _ProjectPageState extends State<ProjectPage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,7 +57,7 @@ class _ProjectPageState extends State<ProjectPage> {
                         icon: const Icon(
                           Icons.search,
                           size: 28,
-                          color: Colors.black,
+                          color: Colors.blue,
                         ),
                       ),
                     ),
@@ -68,7 +71,7 @@ class _ProjectPageState extends State<ProjectPage> {
                         icon: const Icon(
                           Icons.share_outlined,
                           size: 28,
-                          color: Colors.black,
+                          color: Colors.blue,
                         ),
                       ),
                     ),
@@ -94,6 +97,126 @@ class _ProjectPageState extends State<ProjectPage> {
               ],
             ),
           ),
+          const Divider(),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(25),
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Files',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      'Create new',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                buildFileRow('Assets', true),
+                buildFileRow('Wireframes', false),
+                buildFileRow('Hi-Fi mocks', false),
+                buildFileRow('3D designs', false),
+                buildFileRow('Animations', false),
+                buildFileRow('Research', false),
+                buildFileRow('Prototype', false),
+                buildFileRow('Other', false),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Container(
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white,
+              spreadRadius: 7,
+              blurRadius: 1,
+            ),
+          ],
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          currentIndex: selectedIndex,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_time), label: 'Access time'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_box), label: 'Add folder'),
+          ]),
+    );
+  }
+
+  Container buildFileRow(String folderName, bool showAlert) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      height: 65,
+      decoration: BoxDecoration(
+          color: Colors.grey.shade200, borderRadius: BorderRadius.circular(12)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Icon(
+                    Icons.folder,
+                    color: Colors.blue[200],
+                  ),
+                  if (showAlert)
+                    Positioned(
+                      right: 0,
+                      top: 2,
+                      child: Container(
+                        padding: const EdgeInsets.all(2),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: const CircleAvatar(
+                          radius: 3.5,
+                          backgroundColor: Colors.red,
+                        ),
+                      ),
+                    )
+                ],
+              ),
+              const SizedBox(width: 12),
+              Text(
+                folderName,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.more_vert_rounded)),
         ],
       ),
     );
@@ -101,7 +224,7 @@ class _ProjectPageState extends State<ProjectPage> {
 
   Widget buildAvatar(String name, String image) {
     return Padding(
-      padding: const EdgeInsets.only(right: 10),
+      padding: const EdgeInsets.only(right: 9),
       child: Column(
         children: [
           CircleAvatar(
